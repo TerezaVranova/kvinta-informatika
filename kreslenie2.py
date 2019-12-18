86d23f1f59be7287b3f43cb45b69149d12cda1a6
TerezaVranova <56917459+TerezaVranova@users.noreply.github.com>
2019-12-18 12:24:51 +0100

    Add files via upload

--- kreslenie2.py
+++ kreslenie2.py
@@ -4,24 +4,19 @@ x_max,y_max=800,600
 canvas=tkinter.Canvas(width=x_max,height=y_max,bg="white")
 canvas.pack()
 
-xx,yy=0,0
+x1,y1=0,0
+
+def klik(event):
+    x,y=event.x,event.y
 
 def kreslenie(event):
-    x,y=event.x,event.y
-    r=1
-    canvas.create_oval(x-r,y-r,x+r,y+r,fill="black")
-    global xx,yy
-    xx,yy=x,y
-
-x1,y1=0,0
-def kreslenie2(event):
     x,y=event.x,event.y
     global x1,y1
     canvas.create_line(x1,y1,x,y,width=2)
     x1,y1=x,y
 
-#canvas.bind("<ButtonPress>",)
-canvas.bind("<Motion>",kreslenie2)
+#canvas.bind("<ButtonPress>",klik)
+canvas.bind("<Motion>",kreslenie)
 #canvas.bind("<ButtonRelease>",)
 
     
