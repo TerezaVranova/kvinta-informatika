9b620adc772d9114f5871af7fe953872827f9171
TerezaVranova <56917459+TerezaVranova@users.noreply.github.com>
2020-01-06 12:19:48 +0100

    Update kreslenie2.py

--- kreslenie2.py
+++ kreslenie2.py
@@ -7,7 +7,8 @@ canvas.pack()
 x1,y1=0,0
 
 def klik(event):
-    x,y=event.x,event.y
+    global x1,y1
+    x1,y1=event.x,event.y
 
 def kreslenie(event):
     x,y=event.x,event.y
@@ -15,8 +16,5 @@ def kreslenie(event):
     canvas.create_line(x1,y1,x,y,width=2)
     x1,y1=x,y
 
-#canvas.bind("<ButtonPress>",klik)
-canvas.bind("<Motion>",kreslenie)
-#canvas.bind("<ButtonRelease>",)
-
-    
+canvas.bind("<Button-1>",klik)
+canvas.bind("<B1-Motion>",kreslenie)
